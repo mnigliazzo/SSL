781b3a99d3d8148c2cef13a58bba025c4c63ecda
Matias Nigliazzo <mnigliazzo@est.frba.utn.edu.ar>
2020-07-19 20:17:27 -0300

    Actualizo FSM

--- 05-wl/wl-3-rec.c
+++ 05-wl/wl-3-rec.c
@@ -8,12 +8,7 @@
 
 void Funcion_IN(FILE *entrada, FILE *salida);
 void Funcion_OUT(FILE *entrada, FILE *salida);
-typedef enum
-{
-  IN,
-  OUT
-} State;
-State state = OUT;
+
 int main()
 {
   FILE *archivoSalida, *archivoEntrada;
@@ -21,46 +16,42 @@ int main()
   archivoEntrada = fopen("entrada_wl.txt", "r");
   Funcion_OUT(archivoEntrada, archivoSalida);
 }
-int c;
-void Funcion_IN(FILE *entrada, FILE *salida)
-{
-  switch (c)
-  {
-  case EOF:
-    return;
-  case ' ':
-  case '\t':
-  case '\n':
-    return;//Funcion_OUT(entrada, salida);
-  default:
-    if (state == IN)
-    {
-      putc(c, salida);
 
-      state = OUT;
-    }
-    Funcion_OUT(entrada, salida);
-  }
-}
+
 
 void Funcion_OUT(FILE *entrada, FILE *salida)
 {
- 
-  c = fgetc(entrada);
-  switch (c)
+int c;
+  switch (c= fgetc(entrada))
   {
   case EOF:
     return;
   case ' ':
   case '\t':
   case '\n':
-    if (state == OUT)
-    {
-      putc('\n', salida);
-    }
+     Funcion_OUT(entrada, salida);
+      break;
   default:
-    state = IN;
+    putc(c, salida);
     Funcion_IN(entrada, salida);
+  }
+}
+
+void Funcion_IN(FILE *entrada, FILE *salida)
+{
+  int c;
+  switch (c= fgetc(entrada))
+  {
+  case EOF:
+    return;
+  case ' ':
+  case '\t':
+  case '\n':
+    putc('\n', salida);
     Funcion_OUT(entrada, salida);
+    break;
+  default:
+    putc(c, salida);
+    Funcion_IN(entrada, salida);
   }
 }
